4d4825c44b855acb70633c43106882d8e79dd73b
Terrnox <81361890+Terrnox@users.noreply.github.com>
2023-12-20 11:13:32 +0100

    Add files via upload

---
+++ Nowy folder/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using ByteCuisine.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ByteCuisine.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public async Task<ActionResult<List<Account>>> GetAccountList()
+        {
+            var list = new List<Account>();
+
+            return Ok(list);
+        }
+    }
+}
---
+++ Nowy folder/Controllers/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ByteCuisine.Server.Controllers.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        
+    }
+}
