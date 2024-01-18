09d871c8767dc759ff7ffa1bdee78914ebcbaea4
Michał Biesiadecki <michal.biesiadecki4@gmail.com>
2024-01-18 18:58:04 +0100

    Zmiany w Fridge oraz MyNavigation - Dodano zakładkę Admin, która wyświetla się w bocznej nawigacji tylko jeśli zalogowany użytkownik ma role Admin - Dodano poprawne zaznaczanie pojedynczego przepisu - Dodano możliwość jednoczesnego zaznaczania składników i wyszukiwania - Dodano możliwość jednoczesnego zaznaczania przepisu i wyszukiwania - Dodano czyszczenie zmiennych w UserState w celu braku zachowania przywileju admina

--- Client/Services/UserStateService.cs
+++ Client/Services/UserStateService.cs
@@ -5,12 +5,27 @@ namespace ByteCuisine.Client.Services
     public class UserStateService
     {
         public int CurrentUser { get; private set; }
+        public string? CurrentUserType { get; private set; }
 
         public event Action OnChange;
 
         public void SetCurrentUser(int user)
         {
             CurrentUser = user;
+            NotifyStateChanged();
+        }
+
+        public void SetCurrentUser(int user, string user_type)
+        {
+            CurrentUser = user;
+            CurrentUserType = user_type;
+            NotifyStateChanged();
+        }
+
+        public void Logout()
+        {
+            CurrentUser = 0;
+            CurrentUserType = null;
             NotifyStateChanged();
         }
 
