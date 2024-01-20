660de1ead985f9eea2c2274d490ec8c47fddd029
Michał Biesiadecki <michal.biesiadecki4@gmail.com>
2024-01-20 20:27:12 +0100

    Zmiany w zakładce Admin - Dodano wybieranie dowolnego pliku .csv w importowaniu plików - Dodano odpowiednie przyciski oraz ich style

---
+++ Client/wwwroot/scripts/selectFile.js
@@ -0,0 +1,25 @@
+﻿function readFileInfo(inputElement) {
+    if (inputElement.files.length === 0) {
+        return null;
+    }
+    const file = inputElement.files[0];
+    return {
+        name: file.name,
+        size: file.size,
+        type: file.type,
+        lastModified: file.lastModified
+    };
+}
+
+function readFileAsText(inputElement) {
+    return new Promise((resolve, reject) => {
+        if (inputElement.files.length === 0) {
+            reject("No file selected");
+        }
+        const file = inputElement.files[0];
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsText(file);
+    });
+}
---
+++ Shared/FileInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteCuisine.Shared
+{
+    public class FileInfoModel
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public string Type { get; set; }
+        public long LastModified { get; set; }
+    }
+}
