4b15d0f0a3e82eee60d1973a46209e4ebe5516f1
Michał Biesiadecki <michal.biesiadecki4@gmail.com>
2024-01-20 22:08:21 +0100

    Dodano poprawne ładowanie danych do wykresu w zakładce Statistics

---
+++ Client/wwwroot/scripts/chart-interactions.js
@@ -0,0 +1,8 @@
+﻿function destroyChart(chartId) {
+    var chart = Chart.getChart(chartId);
+    if (chart) {
+        chart.destroy();
+    }
+}
+
+window.destroyChart = destroyChart; // Eksportuj funkcję do globalnego zakresu
