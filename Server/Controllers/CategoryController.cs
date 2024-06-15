7d677c214fe0a5b6cf866a4ba83bb550a4aa4b31
Michał Biesiadecki <michal.biesiadecki4@gmail.com>
2024-06-15 15:08:42 +0200

    Dodano stronę AddRecipe w której użytkownik może dodać własny przepis

---
+++ Server/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using ByteCuisine.Server.Controllers.Data;
+using ByteCuisine.Shared.DTOs;
+using ByteCuisine.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ByteCuisine.Server.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+
+        public CategoryController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> Get()
+        {
+            try
+            {
+                var list = await _dataContext.Categories.ToListAsync();
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+    }
+}
